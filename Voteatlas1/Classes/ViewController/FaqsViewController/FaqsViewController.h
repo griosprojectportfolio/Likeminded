03282c90e305147000c3539bca191bf2188adbcd
Ritesh <rkumar.iar@gmail.com>
2015-11-30 17:35:09 +0530

    Added Faqs in app.

--- Voteatlas1/Classes/ViewController/FaqsViewController/FaqsViewController.h
+++ Voteatlas1/Classes/ViewController/FaqsViewController/FaqsViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface FaqsViewController : UIViewController
-
+@property (nonatomic, strong) IBOutlet UIWebView *wvFaqs;
 @end
--- Voteatlas1/Classes/ViewController/FaqsViewController/FaqsViewController.m
+++ Voteatlas1/Classes/ViewController/FaqsViewController/FaqsViewController.m
@@ -9,36 +9,34 @@
 #import "FaqsViewController.h"
 
 @interface FaqsViewController ()
-@property(nonatomic,strong)AppApi *api;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) NSString *htmlString;
 @end
 
 @implementation FaqsViewController
+@synthesize htmlString, wvFaqs, activityIndicator;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.api = [AppApi sharedClient];
-    
     self.title = @"FAQ's";
     self.navigationController.navigationBar.titleTextAttributes = @ {NSForegroundColorAttributeName:[UIColor setCustomColorOfTextField]};
-
     [self setDefaultSeetingOfContent];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.translucent = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    
-    [super viewWillAppear:animated];
-    self.navigationController.navigationBar.translucent = YES;
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    
-    [super viewDidAppear:animated];
-}
 
 #pragma mark - Set Default of content
 - (void)setDefaultSeetingOfContent {
@@ -56,11 +54,50 @@
     UIBarButtonItem *btnBack = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"back"] style:UIBarButtonItemStylePlain target:self action:@selector(backBtnTapped)];
     btnBack.tintColor = [UIColor setCustomColorOfTextField];
     self.navigationItem.leftBarButtonItem = btnBack;
+
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"faq" ofType:@"html"];
+    htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+    wvFaqs.backgroundColor = [UIColor clearColor];
+    [wvFaqs loadHTMLString:htmlString baseURL:nil];
+
 }
 
 #pragma mark - Back btn tapped
 - (void)backBtnTapped {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - Webview delegate methods
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [self addActivityIndicator];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self stopAnimation];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    [self stopAnimation];
+}
+
+
+#pragma mark - Add activity indicator
+
+- (void)addActivityIndicator {
+    
+    activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectZero];
+    activityIndicator.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    activityIndicator.color = [UIColor setCustomDarkBlueColor];
+    [wvFaqs bringSubviewToFront:activityIndicator];
+    [wvFaqs addSubview:activityIndicator];
+    [activityIndicator startAnimating];
+}
+
+- (void)stopAnimation {
+    [activityIndicator stopAnimating];
+    [activityIndicator removeFromSuperview];
 }
 
 @end
